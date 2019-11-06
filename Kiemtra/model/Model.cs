6d537e1f412d85c1ce5b530b6996f22353d35b33
khaihqth1807028 <khaihqth1807028@fpt.edu.vn>
2019-11-06 16:03:28 +0700

    gui bai

---
+++ Kiemtra/model/Model.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLitePCL;
+
+namespace Kiemtra.model
+{
+    class Model
+    {
+        private const string DatabaseName = "khaiho.db";
+
+        private static Model _instance;
+        public SQLiteConnection Connection { get; }
+
+        public static Model GetIntances()
+        {
+            if (_instance == null)
+            {
+                _instance = new Model();
+            }
+            return _instance;
+        }
+
+        private Model
+            ()
+        {
+            Connection = new SQLiteConnection(DatabaseName);
+            CreateTables();
+        }
+
+        private void CreateTables()
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS Customer (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Name VARCHAR( 140 ),PhoneNumber VARCHAR( 140 ));";
+            using (var statement = Connection.Prepare(sql))
+            {
+                statement.Step();
+            }
+            sql = @"CREATE TABLE IF NOT EXISTS Student (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Name VARCHAR( 140 ));";
+            using (var statement = Connection.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+
+        public void InsertDemo()
+        {
+            try
+            {
+                using (var custstmt = Connection.Prepare("INSERT INTO Customer (Name, PhoneNumber) VALUES (?, ?)"))
+                {
+                    custstmt.Bind(1, "Tung");
+                    custstmt.Bind(2, "091777777");
+                    custstmt.Step();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // TODO: Handle error}
+
+            }
+        }
+    }
+}
